d17c17f4cb31b191d727c2448bf825b7b9770a9a
veronika sukantha <85723282+veronika-sukantha@users.noreply.github.com>
2023-07-27 13:44:54 +0530

    task5-2.js

--- task5-2.js
+++ task5-2.js
@@ -1,4 +1,4 @@
 const squareFunction=require('./task5-1');
 const number=5;
 const result=squareFunction(number);
-console.log('The Square Of ${number} is:${result}');
+console.log(`The Square Of ${number} is:${result}`);
